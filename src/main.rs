22cb17a4f60ab9ef11dae662e281822c12e34873
ricardomedeirosdacostajunior <ricardo.medeiros@pismo.io>
2021-02-16 13:50:08 -0300

    Add example of Shared Mutable State
    Signed-off-by: ricardomedeirosdacostajunior <ricardo.medeiros@pismo.io>

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,30 @@
-use actix_web::{get, web, App, HttpServer};
+use actix_web::{web, App, HttpServer};
+use std::sync::Mutex;
 
 static ADDRESS: &str = "127.0.0.1";
 static PORT: &str = "8080";
 
-struct AppState {
-    app_name :String,
+struct AppStateWithCounter {
+    counter :Mutex<i32>,
 }
 
-#[get("/")]
-async fn index(data: web::Data<AppState>) -> String {
-    let app_name = &data.app_name;
+async fn index(data: web::Data<AppStateWithCounter>) -> String {
+    let mut counter = data.counter.lock().unwrap();
+    *counter += 1;
 
-    format!("Hello {}!", app_name)
+    format!("Request number: {}", counter)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let counter = web::Data::new(AppStateWithCounter{
+        counter: Mutex::new(0),
+    });
+
+    HttpServer::new(move || {
         App::new()
-            .data(AppState {
-                app_name: String::from("Actix-web")
-            })
-            .service(index)
+            .app_data(counter.clone())
+            .route("/", web::get().to(index))
     })
         .bind(format!("{}:{}", ADDRESS, PORT))?
         .run()
