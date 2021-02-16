b5233f90b092670498191f30d49874da27b2e46e
ricardomedeirosdacostajunior <ricardo.medeiros@pismo.io>
2021-02-16 13:31:54 -0300

    Add hello world example
    Signed-off-by: ricardomedeirosdacostajunior <ricardo.medeiros@pismo.io>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+static ADDRESS: &str = "127.0.0.1";
+static PORT: &str = "8080";
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+        .bind(format!("{}:{}", ADDRESS, PORT))?
+        .run()
+        .await
+}
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
 }
