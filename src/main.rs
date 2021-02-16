8abffded0c8e1da18a383c6809ff7cdd16f33c9e
ricardomedeirosdacostajunior <ricardo.medeiros@pismo.io>
2021-02-16 13:42:32 -0300

    Add state example
    Signed-off-by: ricardomedeirosdacostajunior <ricardo.medeiros@pismo.io>

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,29 @@
-// use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use actix_web::{web, App, HttpServer, Responder};
+use actix_web::{get, web, App, HttpServer};
 
 static ADDRESS: &str = "127.0.0.1";
 static PORT: &str = "8080";
+
+struct AppState {
+    app_name :String,
+}
+
+#[get("/")]
+async fn index(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name;
+
+    format!("Hello {}!", app_name)
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(
-                web::scope("/app")
-                    .route("/index.html", web::get().to(index)),
-            )
+            .data(AppState {
+                app_name: String::from("Actix-web")
+            })
+            .service(index)
     })
         .bind(format!("{}:{}", ADDRESS, PORT))?
         .run()
         .await
 }
-
-async fn index() -> impl Responder {
-    "Hello world!"
-}
-
-// #[get("/")]
-// async fn hello() -> impl Responder {
-//     HttpResponse::Ok().body("Hello world!")
-// }
-//
-// #[post("/echo")]
-// async fn echo(req_body: String) -> impl Responder {
-//     HttpResponse::Ok().body(req_body)
-// }
-//
-// async fn manual_hello() -> impl Responder {
-//     HttpResponse::Ok().body("Hey there!")
-// }
